fcf91563a28967e7517934900b0620c1958d0413
muujin <mujin623@cau.ac.kr>
2024-11-02 22:01:25 +0900

    #6.7 Stateful Navigation part Two
    - Offstage : 위젯이 안보이게하면서 계속 존재하게 함(stack)

--- lib/screens/features/main_navigation/main_navigation_screen.dart
+++ lib/screens/features/main_navigation/main_navigation_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/features/main_navigation/stf_screen.dart';
 import 'package:tiktok_clone/screens/features/main_navigation/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
@@ -14,34 +15,6 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
-  final screens = [
-    const Center(
-      child: Text(
-        'Home',
-        style: TextStyle(fontSize: 49),
-      ),
-    ),
-    const Center(
-      child: Text(
-        'Discover',
-        style: TextStyle(fontSize: 49),
-      ),
-    ),
-    Container(),
-    const Center(
-      child: Text(
-        'Inbox',
-        style: TextStyle(fontSize: 49),
-      ),
-    ),
-    const Center(
-      child: Text(
-        'Profile',
-        style: TextStyle(fontSize: 49),
-      ),
-    ),
-  ];
-
   void _onTap(int index) {
     setState(() {
       _selectedIndex = index;
@@ -51,7 +24,24 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens.elementAt(_selectedIndex),
+      body: Stack(children: [
+        Offstage(
+          offstage: _selectedIndex != 0,
+          child: const StfScreen(),
+        ),
+        Offstage(
+          offstage: _selectedIndex != 1,
+          child: const StfScreen(),
+        ),
+        Offstage(
+          offstage: _selectedIndex != 3,
+          child: const StfScreen(),
+        ),
+        Offstage(
+          offstage: _selectedIndex != 4,
+          child: const StfScreen(),
+        ),
+      ]),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Row(
