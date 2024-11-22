9f6e3be7b2503177c3b193c73dc8f3d181bec11d
muujin <mujin623@cau.ac.kr>
2024-11-22 19:00:49 +0900

    #8.0 showModalBottomSheet
    - showModalBottomSheet → context : 인자로 받게 하고, build : 임시로 Container() 넣기
        - future를 return한다는  얘기는 await/async가 가능하다는 얘기 → 이 위젯은 닫는것으로 await이 resolve됨
        - barrier color : 위 배경 색상
        - background color : 댓글창 색상
    - automaticallyImplyLeading: false → Appbar에서 뒤로가기 버튼 없애기
    - Navigator.of(context).pop() → Appbar에서 방금 전 화면으로 돌아가는 메소드
    - Container - clipBehavior: Clip.hardEdge → decoration에서 값을 변경하는 것을 반영해줌

--- lib/screens/features/videos/widgets/video_post.dart
+++ lib/screens/features/videos/widgets/video_post.dart
@@ -1,8 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/screens/features/videos/widgets/video_button.dart';
+import 'package:tiktok_clone/screens/features/videos/widgets/video_comments.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
@@ -75,7 +78,9 @@ class _VideoPostState extends State<VideoPost>
   }
 
   void _onVisibilityChanged(VisibilityInfo info) {
-    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+    if (info.visibleFraction == 1 &&
+        !_isPaused &&
+        !_videoPlayerController.value.isPlaying) {
       _videoPlayerController.play();
     }
   }
@@ -99,6 +104,18 @@ class _VideoPostState extends State<VideoPost>
     setState(() {
       _isTagExpanded = !_isTagExpanded;
     });
+  }
+
+  void _onCommentsTap(BuildContext context) async {
+    if (_videoPlayerController.value.isPlaying) {
+      _onTogglePause();
+    }
+    await showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (context) => const VideoComments(),
+    );
+    _onTogglePause();
   }
 
   @override
@@ -202,12 +219,12 @@ class _VideoPostState extends State<VideoPost>
               ],
             ),
           ),
-          const Positioned(
+          Positioned(
             bottom: 20,
             right: 10,
             child: Column(
               children: [
-                CircleAvatar(
+                const CircleAvatar(
                   radius: 25,
                   backgroundColor: Colors.black,
                   foregroundColor: Colors.white,
@@ -216,19 +233,22 @@ class _VideoPostState extends State<VideoPost>
                   child: Text("MJ"),
                 ),
                 Gaps.v24,
-                VideoButton(
+                const VideoButton(
                   icon: FontAwesomeIcons.solidHeart,
                   text: "2.9M",
                 ),
                 Gaps.v24,
-                VideoButton(
-                  icon: FontAwesomeIcons.solidComment,
-                  text: "2.9M",
+                GestureDetector(
+                  onTap: () => _onCommentsTap(context),
+                  child: const VideoButton(
+                    icon: FontAwesomeIcons.solidComment,
+                    text: "33K",
+                  ),
                 ),
                 Gaps.v24,
-                VideoButton(
+                const VideoButton(
                   icon: FontAwesomeIcons.share,
-                  text: "2.9M",
+                  text: "Share",
                 ),
               ],
             ),
